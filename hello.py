7b8eddf80d3c33303677f89e88708a25ccbb8edb
psyoblade <psyoblade@ncsoft.com>
2018-07-17 08:44:30 +0900

    generator test code

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
-print "hello world"
+def generate():
+    M = [[1,2,3],[4,5,6],[7,8,9]]
+    G = (sum(row) for row in M)
+    return G
+
+def main():
+    G = generate()
+    print(next(G))
+    print(next(G))
+    print(next(G))
+
+if __name__ == "__main__":
+    main()
