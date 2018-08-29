0f84bb7d64c44e2af10324c0eace4b68ee6ece53
psyoblade <psyoblade@ncsoft.com>
2018-08-30 08:41:05 +0900

    add comprehension examples

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+TODO_2018_08_28 = """
+https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Comprehensions.html
+https://docs.python.org/3.6/library/timeit.html
+https://docs.python.org/3.6/library/debug.html
+https://docs.python.org/3.6/library/collections.html
+"""
 
 def generate():
     M = [[1,2,3],[4,5,6],[7,8,9]]
